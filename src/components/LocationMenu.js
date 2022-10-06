1b712d3257a5bb82086de8a7952dbcccd4a74070
Mathias Moen <mathiasmoen@dhcp-10-22-114-34.wlan.ntnu.no>
2022-10-06 17:35:20 +0200

    small changes

--- src/components/LocationMenu.js
+++ src/components/LocationMenu.js
@@ -11,7 +11,7 @@ const menuStyle = {
 
 const locations = [
   { center: [10.674706290112326, 59.92544400487645], name: "Skøyen" },
-  { center: [10.408773, 63.422091], name: "Trondheim" },
+  { center: [10.408773, 63.422091], name: "Trondhjæm" },
   { center: [7.993744574800331, 58.145072155868036], name: "Kristiansand" },
   { center: [10.457060447232827, 61.123876982347184], name: "LilleHammer" },
   { center: [5.341447457437576, 60.372653021429045], name: "Bergen" },
